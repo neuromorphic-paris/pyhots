5058e065ef88df4dd4496a2d6c3f679103148cfe
Gregor Lenz <mail@lenzgregor.com>
2019-10-07 21:42:51 +0200

    add linear decay

--- TimeSurface.py
+++ TimeSurface.py
@@ -3,12 +3,15 @@ import ipdb
 
 
 class TimeSurface:
-    def __init__(self, layer, timestamp_context):
-        timestamp_data = np.exp(timestamp_context/layer.tau)
-        timestamp_data[timestamp_context < (-3*layer.tau)] = 0
-        if timestamp_context.shape != (layer.polarities, 2 * layer.radius
-                                       + 1, 2 * layer.radius + 1):
-            ipdb.set_trace()
+    def __init__(self, layer, timestamp_context, decay='lin'):
+        if decay == 'lin':
+            timestamp_data = timestamp_context / (3 * layer.tau) + 1
+            timestamp_data[timestamp_data < 0] = 0
+        elif decay == 'exp':
+            timestamp_data = np.exp(timestamp_context/layer.tau)
+            timestamp_data[timestamp_context < (-3*layer.tau)] = 0
+        assert timestamp_context.shape == (layer.polarities, 2 * layer.radius
+                                           + 1, 2 * layer.radius + 1)
         self.data = timestamp_data
 
     def normalize(self):
