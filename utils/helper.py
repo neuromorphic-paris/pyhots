e5e655b86f634539d04d07f0e5c030a457501c5d
Gregor Lenz <mail@lenzgregor.com>
2019-11-15 23:39:01 +0100

    improve readability of cluster center plotting

--- utils/helper.py
+++ utils/helper.py
@@ -6,10 +6,12 @@ def plot_centers(centers, activations):
     edge_length = np.sqrt(len(centers)).astype(int)
     fig, axes = plt.subplots(edge_length, edge_length)
     axes = np.reshape(axes, -1)
+    total_activation = np.sum(activations)
+    total_clusters = len(centers)
 
     for index, axis in enumerate(axes):
         axisImag = axis.imshow(centers[index], vmin=0, vmax=1, cmap = plt.cm.hot, interpolation = 'none', origin = 'upper')
-        axis.set_ylabel(str(int(activations[index])), rotation=0, labelpad=25)
+        axis.set_ylabel(str(round(activations[index]/total_activation*total_clusters, 1)), rotation=0, labelpad=17)
         axis.set_xticks([])
         axis.set_xticklabels('')
         axis.set_yticks([])
